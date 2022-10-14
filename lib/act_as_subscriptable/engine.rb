910770f231e2ddac5a5c580297e0a06f22615766
Dylan Lin <dylanmail0203@gmail.com>
2022-10-14 22:20:45 +0800

    🔧 rspec settings

--- lib/act_as_subscriptable/engine.rb
+++ lib/act_as_subscriptable/engine.rb
@@ -1,5 +1,9 @@
 module ActAsSubscriptable
   class Engine < ::Rails::Engine
     isolate_namespace ActAsSubscriptable
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
